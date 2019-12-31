60c98946d791b67368b31fd94e9d0301ee7d789b
Armando Jr <elfiservice@hotmail.com>
2019-12-31 18:25:28 -0300

    feat: implemented App container using action

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,17 +1,50 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import { connect } from 'react-redux'
+import { getCurrentLocationOfUser, mountMapToDeveloperSelected } from '../actions/appActions'
+import { loadGoogleApi } from '../utils/googleApi'
+import SearchForm from '../components/SearchForm'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> 
-        </p>
 
-      </header>
-    </div>
-  );
+class App extends Component {
+  componentDidMount() {
+    loadGoogleApi()
+      .then(result => {
+        this.props.getCurrentLocationOfUser()
+      })
+  }
+
+  // componentDidUpdate(newProps) {
+  //   console.log(this.props.appData.currentLocationOfUser);
+  //   console.log(newProps.appData.currentLocationOfUser);
+  //   if(this.props.appData.currentLocationOfUser !== newProps.appData.currentLocationOfUser) {
+  //     this.props.mountMapToDeveloperSelected('Fortaleza - CE')
+  //   }
+    
+  // }
+
+  render() {
+      const { appData } = this.props
+      return (
+        <div className="App">
+          <header className="App-header">
+            <h1>Quick GitHub Users</h1>
+            <p>
+                Location <code>{appData.currentLocationOfUser !== null 
+                  ?  appData.currentLocationOfUser : 'getting current position...'}</code> 
+            </p>
+          </header>
+          <SearchForm />
+          <div id="mapHidden"></div>
+      </div>
+      )
+  }
 }
 
-export default App;
+function mapStateToProps ({ appData }) {
+  return {
+    appData
+  }
+}
+
+export default connect(mapStateToProps, { getCurrentLocationOfUser, mountMapToDeveloperSelected })(App)
