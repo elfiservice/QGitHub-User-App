2dc35b72d9989a780969d2c2538dbf65d78683e9
Armando Jr <elfiservice@hotmail.com>
2019-12-31 11:09:44 -0300

    first commit

---
+++ src/containers/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> 
+        </p>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import '../node_modules/font-awesome/css/font-awesome.min.css'
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import App from './containers/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import promise from 'redux-promise'
+import { applyMiddleware ,createStore } from 'redux'
+import { Provider } from 'react-redux'
+import reducers from './reducers'
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const store = applyMiddleware(promise)(createStore)(reducers)
+
+ReactDOM.render(
+    <Provider store={store}><App /></Provider>,
+    document.getElementById('root'));
+
+
