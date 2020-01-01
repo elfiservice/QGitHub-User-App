80e553a5c14a78278bd190125606013b5977deee
Armando Jr <elfiservice@hotmail.com>
2020-01-01 12:07:10 -0300

    refactore: get users in github Api functions

--- src/utils/githubApi.js
+++ src/utils/githubApi.js
@@ -4,15 +4,23 @@ const headers = {
     'Accept': 'application/json',
   }
 
-export const searchUsers = (query) =>
-  fetch(`${api}/search/users?q=fullname:${query}+user:${query}`, {
-    method: 'POST',
-    headers: {
-      ...headers,
-      'Content-Type': 'application/json'
-    },
-    // body: JSON.stringify({ query })
-  }).then(res => res.json())
+export const searchUsersByFullname = (query) =>
+  fetch(`${api}/search/users?q=fullname:${query}`)
+    .then(res => res.json())
+
+export const searchUsersByLogin = (query) =>
+  fetch(`${api}/search/users?q=user:${query}`)
+    .then(res => res.json())
+
+// export const searchUsers = (query) =>
+//   fetch(`${api}/search/users?q=fullname:${query}+user:${query}`, {
+//     method: 'POST',
+//     headers: {
+//       ...headers,
+//       'Content-Type': 'application/json'
+//     },
+//     // body: JSON.stringify({ query })
+//   }).then(res => res.json())
 
 export const getUserData = (loginUser) =>
   fetch(`${api}/users/${loginUser}`, { headers })
