54644e9e0d6e41a40b5109baa88dd3b86dcef4bd
Armando Jr <elfiservice@hotmail.com>
2020-01-05 10:23:08 -0300

    Updates

---
+++ precache-manifest.eda2087f8735d6ae96b9e4e1335d1e78.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e36e9ba45e9a9e541d4a201aa3b54cd",
+    "url": "/QGitHub-User-App/index.html"
+  },
+  {
+    "revision": "42c789cb2f30b7b3ebec",
+    "url": "/QGitHub-User-App/static/css/2.ba013e49.chunk.css"
+  },
+  {
+    "revision": "d8b7356d445200860079",
+    "url": "/QGitHub-User-App/static/css/main.0d140265.chunk.css"
+  },
+  {
+    "revision": "42c789cb2f30b7b3ebec",
+    "url": "/QGitHub-User-App/static/js/2.c7188521.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/QGitHub-User-App/static/js/2.c7188521.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d8b7356d445200860079",
+    "url": "/QGitHub-User-App/static/js/main.ce9f744e.chunk.js"
+  },
+  {
+    "revision": "6ef61f8fdf609ba4b833",
+    "url": "/QGitHub-User-App/static/js/runtime-main.86b2be00.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/QGitHub-User-App/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/QGitHub-User-App/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/QGitHub-User-App/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/QGitHub-User-App/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/QGitHub-User-App/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
