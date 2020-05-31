d65e5a28613a3a763ab77c8a0001dcac2796fae5
Armando Jr <elfiservice@hotmail.com>
2020-05-31 11:40:02 -0300

    removed redux-thunk

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,6 +1,5 @@
 import { applyMiddleware } from 'redux'
 import logger from './logger'
-import thunk from 'redux-thunk'
 import createSagaMiddleware from 'redux-saga'
 import rootSaga from '../sagas'
 import reducer from '../reducers'
@@ -9,7 +8,6 @@ import { createStore } from 'redux'
 const sagaMiddleware = createSagaMiddleware()
 const middleware = applyMiddleware(
     logger,
-    thunk,
     sagaMiddleware
 )
 const store = createStore(reducer, middleware)
